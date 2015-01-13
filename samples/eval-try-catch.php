4d13d3274a8eea45db49186b760c92e02afb936c
Malicius <malicius@infectious.com>
2015-01-12 18:06:32 -0800

    New samples and scripts

---
+++ samples/eval-try-catch.php
@@ -0,0 +1,41 @@
+<?php
+
+function divide($a, $b) {
+  if ($b == 0)
+    throw new Exception("Division by zero!\n");
+  return $a / $b;
+}
+
+try {
+  $num = rand(0, 6);
+  while (true) {
+    if ($num < 0) {
+      print "Num is a foobar!\n";
+    } else {
+      if ($num < 5) {
+        switch ($num) {
+        case 1: 
+          print "foo\n";
+        case 2: 
+          print "bar " . divide($num, 0) . "\n";
+        case 3: 
+          print "zot\n";
+        case 4: 
+          eval('try { print "nonsense: " . divide(4, 0) . "\n"; } catch (Exception $e) { print "Exception in eval: " . $e . "\n"; }'); 
+          break;
+        }
+      } else {
+        switch ($num) {
+        case 5: print "zee\n";
+        case 6: print "zoo\n";
+        }
+      }
+    }
+    $num--;
+  }
+} catch (Exception $e) {
+  print("Error: " . $e . "\n");
+  eval('try { print "nonsense: " . divide(4, 0) . "\n"; } catch (Exception $e) { print "Exception in eval: " . $e . "\n"; }'); 
+}
+
+?>
---
+++ samples/try-catch.php
@@ -0,0 +1,35 @@
+<?php
+
+function divide($a, $b) {
+  if ($b == 0)
+    throw new Exception("Division by zero!\n");
+  return $a / $b;
+}
+
+try {
+  $num = rand(0, 6);
+  while (true) {
+    if ($num < 0) {
+      print "Num is a foobar!\n";
+    } else {
+      if ($num < 5) {
+        switch ($num) {
+        case 1: print "foo\n";
+        case 2: print "bar\n";
+        case 3: print "zot\n";
+        case 4: print "nonsense: " . divide($num, 0);
+        }
+      } else {
+        switch ($num) {
+        case 5: print "zee\n";
+        case 6: print "zoo\n";
+        }
+      }
+    }
+    $num--;
+  }
+} catch (Exception $e) {
+  print("Error: " . $e . "\n");
+}
+
+?>
